bee1f9219c5c36dca170a7a07ff51a9d43c41dbb
BerkayAygul <berkaygithub@gmail.com>
2022-12-05 22:26:40 +0300

    Oyuncu Olusturuldu
    - Seviye sahnesi oluşturuldu.
    
    - Oyuncu oluşturuldu. Oyuncuya örnek beden, silah, gölge eklendi.
    
    - Rigidbody2D ve Circle Collider 2D eklendi.
    
    - Oyuncunun hareketi ve rotasyonu sağlandı.
    
    - Oyuncuya boşta durma ve hareket etme animasyonları eklendi.

---
+++ Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D playerRB;
+
+    public Transform playerWeaponHand;
+
+    private Camera playerCamera;
+
+    public Animator playerAnimator;
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+    }
+
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+
+        playerRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = playerCamera.WorldToScreenPoint(transform.localPosition);
+
+        if(mousePosition.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            playerWeaponHand.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            playerWeaponHand.localScale = Vector3.one;
+        }
+
+        Vector2 weaponOffset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+        float weaponAngle = Mathf.Atan2(weaponOffset.y, weaponOffset.x) * Mathf.Rad2Deg;
+        playerWeaponHand.rotation = Quaternion.Euler(0, 0, weaponAngle);
+
+        if(moveInput != Vector2.zero)
+        {
+            playerAnimator.SetBool("isPlayerMoving", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isPlayerMoving", false);
+        }
+    }
+}
