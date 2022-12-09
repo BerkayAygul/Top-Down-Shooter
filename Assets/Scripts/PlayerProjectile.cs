2a6cd408c66cf0f2d568089ad487b4ea577181e5
BerkayAygul <berkaygithub@gmail.com>
2022-12-09 23:01:22 +0300

    Oyuncu Mermisi Carpisma Efekti Eklendi

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -7,6 +7,8 @@ public class PlayerProjectile : MonoBehaviour
     public float projectileSpeed = 7.5f;
     public Rigidbody2D projectileRB;
 
+    public GameObject projectileImpactEffect;
+
     void Update()
     {
         projectileRB.velocity = transform.right * projectileSpeed;
@@ -14,6 +16,7 @@ public class PlayerProjectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collisionObject)
     {
+        Instantiate(projectileImpactEffect, transform.position, transform.rotation);
         Destroy(gameObject);
     }
 
