727da51c181584eba95c9b8c193a8f86fbdcf942
BerkayAygul <berkaygithub@gmail.com>
2023-03-27 21:20:07 +0300

    Oyuncu Can Göstergesi Eklendi

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class UIController : MonoBehaviour
 
     public GameObject leaderboardTableDisplay;
     public LeaderboardPlayerInformation LeaderboardPlayerInformation;
+
+    public Slider healthSlider;
 
     private void Awake()
     {
